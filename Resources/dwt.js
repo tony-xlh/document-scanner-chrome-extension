13e49e702cdec9af3c9da44413162519f2f6383b
xulihang <xulihanghai@163.com>
2023-02-17 15:23:18 +0800

    add namespace protection

--- Resources/dwt.js
+++ Resources/dwt.js
@@ -1,160 +1,156 @@
-let modal;
-let DWObject;
-
-window.onload = function () {
-  const resourcesURL = document.getElementById("dwt").getAttribute("resourcesURL");
-  Dynamsoft.DWT.ResourcesPath = resourcesURL;
-  addButton(resourcesURL);
-}
-
-function addButton(resourcesURL){
-  const button = document.createElement("div");
-  button.className = "dwt-fab";
-  const a = document.createElement("a")
-  a.href = "javascript:void(0)";
-  const icon = document.createElement("img")
-  icon.src = resourcesURL+"/scanner-scan.svg";
-  a.appendChild(icon);
-  button.appendChild(a);
-  document.body.appendChild(button);
-  button.addEventListener("click", () => {
-    showModal();
-  });
-}
-
-function showModal(){
-  if (!modal) {
-    modal = document.createElement("div");
-    modal.className = "dwt-modal";
-    document.body.appendChild(modal);
-    const header = document.createElement("div");
-    const closeBtn = document.createElement("div");
-    closeBtn.className = "dwt-close-btn";
-    closeBtn.innerText = "x";
-    header.appendChild(closeBtn);
-    header.className = "dwt-header";
-    closeBtn.addEventListener("click", () => {
-      hideModal();
+let DWTChromeExtension = {
+  modal:undefined,
+  DWObject:undefined,
+  load: function(){
+    const resourcesURL = document.getElementById("dwt").getAttribute("resourcesURL");
+    Dynamsoft.DWT.ResourcesPath = resourcesURL;
+    this.addButton(resourcesURL);
+  },
+  addButton: function(resourcesURL){
+    const button = document.createElement("div");
+    button.className = "dwt-fab";
+    const a = document.createElement("a")
+    a.href = "javascript:void(0)";
+    const icon = document.createElement("img")
+    icon.src = resourcesURL+"/scanner-scan.svg";
+    a.appendChild(icon);
+    button.appendChild(a);
+    document.body.appendChild(button);
+    button.addEventListener("click", () => {
+      this.showModal();
     });
-    const body = document.createElement("div");
-    body.className = "dwt-body";
-    const viewer = document.createElement("div");
-    viewer.id = "dwtcontrolContainer";
-    const controls = document.createElement("div");
-    controls.className = "dwt-controls";
-    const scanBtn = document.createElement("button");
-    scanBtn.innerText = "Scan";
-    scanBtn.addEventListener("click", () => {
-      scan();
-    });
-    
-    const copyBtn = document.createElement("button");
-    copyBtn.innerText = "Copy selected";
-    copyBtn.addEventListener("click", () => {
-      copy();
-    });
-
-    const saveBtn = document.createElement("button");
-    saveBtn.innerText = "Save";
-    saveBtn.addEventListener("click", () => {
-      save();
-    });
-
-    const status = document.createElement("div");
-    status.className="dwt-status";
-
-    controls.appendChild(scanBtn);
-    controls.appendChild(copyBtn);
-    controls.appendChild(saveBtn);
-    controls.appendChild(status);
-
-    body.appendChild(viewer);
-    body.appendChild(controls);
-    modal.appendChild(header);
-    modal.appendChild(body);
-    if (!DWObject) {
-      initDWT();
+  },
+  showModal: function(){
+    if (!this.modal) {
+      this.modal = document.createElement("div");
+      this.modal.className = "dwt-modal";
+      document.body.appendChild(this.modal);
+      const header = document.createElement("div");
+      const closeBtn = document.createElement("div");
+      closeBtn.className = "dwt-close-btn";
+      closeBtn.innerText = "x";
+      header.appendChild(closeBtn);
+      header.className = "dwt-header";
+      closeBtn.addEventListener("click", () => {
+        this.hideModal();
+      });
+      const body = document.createElement("div");
+      body.className = "dwt-body";
+      const viewer = document.createElement("div");
+      viewer.id = "dwtcontrolContainer";
+      const controls = document.createElement("div");
+      controls.className = "dwt-controls";
+      const scanBtn = document.createElement("button");
+      scanBtn.innerText = "Scan";
+      scanBtn.addEventListener("click", () => {
+        this.scan();
+      });
+      
+      const copyBtn = document.createElement("button");
+      copyBtn.innerText = "Copy selected";
+      copyBtn.addEventListener("click", () => {
+        this.copy();
+      });
+  
+      const saveBtn = document.createElement("button");
+      saveBtn.innerText = "Save";
+      saveBtn.addEventListener("click", () => {
+        this.save();
+      });
+  
+      const status = document.createElement("div");
+      status.className="dwt-status";
+  
+      controls.appendChild(scanBtn);
+      controls.appendChild(copyBtn);
+      controls.appendChild(saveBtn);
+      controls.appendChild(status);
+  
+      body.appendChild(viewer);
+      body.appendChild(controls);
+      this.modal.appendChild(header);
+      this.modal.appendChild(body);
+      if (!this.DWObject) {
+        this.initDWT();
+      }
     }
-  }
-  document.querySelector(".dwt-fab").style.display = "none";
-  modal.style.display = "";
-}
-
-function hideModal(){
-  modal.style.display = "none";
-  document.querySelector(".dwt-fab").style.display = "";
-}
-
-function scan(){
-  if (DWObject) {
-    if (Dynamsoft.Lib.env.bMobile) {
-      DWObject.Addon.Camera.scanDocument();
-    }else {
-      DWObject.SelectSource(function () {
-        DWObject.OpenSource();
-        DWObject.AcquireImage();
-      },
-        function () {
-          console.log("SelectSource failed!");
-        }
-      );
-    }
-  }
-}
-
-function copy(){
-  if (DWObject) {
-    if (Dynamsoft.Lib.env.bMobile) {
-      DWObject.ConvertToBlob(
-        [DWObject.CurrentImageIndexInBuffer],
-        Dynamsoft.DWT.EnumDWT_ImageType.IT_PNG,
-        function(result) {
-          CopyBlobToClipboard(result);
+    document.querySelector(".dwt-fab").style.display = "none";
+    this.modal.style.display = "";
+  },
+  hideModal: function() {
+    this.modal.style.display = "none";
+    document.querySelector(".dwt-fab").style.display = "";
+  },
+  scan: function(){
+    console.log("scan");
+    if (this.DWObject) {
+      if (Dynamsoft.Lib.env.bMobile) {
+        this.DWObject.Addon.Camera.scanDocument();
+      }else {
+        this.DWObject.SelectSource(function () {
+          DWTChromeExtension.DWObject.OpenSource();
+          DWTChromeExtension.DWObject.AcquireImage();
         },
-        function(errorCode,errorString) {
-          console.log("convert failed");
-          console.log(errorString);
-          alert("Failed");
-        });
-    }else{
-      DWObject.CopyToClipboard(DWObject.CurrentImageIndexInBuffer);
-      alert("Copied");
+          function () {
+            console.log("SelectSource failed!");
+          }
+        );
+      }
     }
+  },
+  copy: function(){
+    if (this.DWObject) {
+      if (Dynamsoft.Lib.env.bMobile) {
+        this.DWObject.ConvertToBlob(
+          [this.DWObject.CurrentImageIndexInBuffer],
+          Dynamsoft.DWT.EnumDWT_ImageType.IT_PNG,
+          function(result) {
+            DWTChromeExtension.CopyBlobToClipboard(result);
+          },
+          function(errorCode,errorString) {
+            console.log("convert failed");
+            console.log(errorString);
+            alert("Failed");
+          });
+      }else{
+        this.DWObject.CopyToClipboard(this.DWObject.CurrentImageIndexInBuffer);
+        alert("Copied");
+      }
+    }
+  },
+  CopyBlobToClipboard: function(blob){
+    var data = [new ClipboardItem({ "image/png": blob})];
+    navigator.clipboard.write(data).then(function() {
+      alert("Copied");
+    }, function() {
+      alert("Failed");
+    });
+  },
+  save: function () {
+    if (this.DWObject) {
+      this.DWObject.SaveAllAsPDF("Scanned");
+    }
+  },
+  initDWT: function(){
+    console.log("initDWT");
+    const status = document.querySelector(".dwt-status");
+    const license = document.getElementById("dwt").getAttribute("license");
+    if (license) {
+      console.log("using license: "+license);
+      Dynamsoft.DWT.ProductKey = license;
+    }
+    Dynamsoft.DWT.Containers = [{ ContainerId: 'dwtcontrolContainer',Width: 270, Height: 350 }];
+    Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', function () {
+      console.log("ready");
+      status.innerText = "";
+      DWTChromeExtension.DWObject = Dynamsoft.DWT.GetWebTwain('dwtcontrolContainer');
+      DWTChromeExtension.DWObject.Viewer.width = "100%";
+      DWTChromeExtension.DWObject.Viewer.height = "100%";
+      DWTChromeExtension.DWObject.SetViewMode(2,2);
+    });
+    status.innerText = "Loading...";
+    Dynamsoft.DWT.Load();
   }
 }
 
-function CopyBlobToClipboard(blob) {
-  var data = [new ClipboardItem({ "image/png": blob})];
-  navigator.clipboard.write(data).then(function() {
-    alert("Copied");
-  }, function() {
-    alert("Failed");
-  });
-}
-
-function save(){
-  if (DWObject) {
-    DWObject.SaveAllAsPDF("Scanned");
-  }
-}
-
-function initDWT(){
-  console.log("initDWT");
-  const status = document.querySelector(".dwt-status");
-  const license = document.getElementById("dwt").getAttribute("license");
-  if (license) {
-    console.log("using license: "+license);
-    Dynamsoft.DWT.ProductKey = license;
-  }
-  Dynamsoft.DWT.Containers = [{ ContainerId: 'dwtcontrolContainer',Width: 270, Height: 350 }];
-  Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', function () {
-    console.log("ready");
-    status.innerText = "";
-    DWObject = Dynamsoft.DWT.GetWebTwain('dwtcontrolContainer');
-    DWObject.Viewer.width = "100%";
-    DWObject.Viewer.height = "100%";
-    DWObject.SetViewMode(2,2);
-  });
-  status.innerText = "Loading...";
-  Dynamsoft.DWT.Load();
-}
+DWTChromeExtension.load();
